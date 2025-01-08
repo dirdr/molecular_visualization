bffc00ad483c4d40fe2c702e95ae57cbc13b80fa
dirdr <adrien.pelfresne@gmail.com>
2025-01-08 21:10:35 +0100

    feat(camera) basic scafholding

--- src/camera.rs
+++ src/camera.rs
@@ -1,36 +1,80 @@
-use nalgebra::{Matrix4, Point3, Vector3};
+use std::f32::consts::PI;
 
-trait Project {
+use nalgebra::{Matrix4, Point3};
+
+/// Define a generic camera contract
+pub trait Camera {
     fn get_view_matrix(&self) -> Matrix4<f32>;
     fn get_projection_matrix(&self) -> Matrix4<f32>;
 }
 
-pub enum Placed {}
-pub enum Pointed {}
+pub trait Place {}
+
+pub trait Point {}
+
+/// Camera that use a perspective projection.
+pub struct PerspectiveCamera<S: CameraState> {
+    fov: f32,
+    aspect_ratio: f32,
+    znear: f32,
+    zfar: f32,
+    state: S,
+}
+
+impl Default for PerspectiveCamera<Virtual> {
+    fn default() -> Self {
+        Self {
+            fov: PI / 3.0,
+            aspect_ratio: 16.0 / 9.0,
+            znear: 0.1,
+            zfar: 1024.0,
+            state: Virtual {},
+        }
+    }
+}
+
+impl PerspectiveCamera<Virtual> {
+    pub fn place(pos: Point3<f32>) -> PerspectiveCamera<Placed> {
+        todo!()
+    }
+
+    pub fn point(target: Point3<f32>, up: Point3<f32>) -> PerspectiveCamera<Pointed> {
+        todo!()
+    }
+}
+
+impl<S> Camera for PerspectiveCamera<S>
+where
+    S: CameraState + Place + Point,
+{
+    fn get_view_matrix(&self) -> Matrix4<f32> {
+        todo!()
+    }
+
+    fn get_projection_matrix(&self) -> Matrix4<f32> {
+        todo!()
+    }
+}
+
+pub struct Placed {
+    pos: Point3<f32>,
+}
+
+pub struct Pointed {
+    target: Point3<f32>,
+    up: Point3<f32>,
+}
+
+/// Define the initial state of the camera,
+/// which is not placed nor pointed, thus not existing in the scene
+pub struct Virtual {}
 
 pub trait CameraState {}
 
 impl CameraState for Placed {}
 impl CameraState for Pointed {}
+impl CameraState for Virtual {}
 
-pub struct Camera<S: CameraState> {
-    pos: Point3<f32>,
-    target: Point3<f32>,
-    up: Vector3<f32>,
-    aspect_ratio: f32,
-    znear: f32,
-    zfar: f32,
-    marker: std::marker::PhantomData<S>,
-}
+impl Place for Placed {}
 
-// impl<S> Camera<S>
-// where
-//     S: CameraState,
-// {
-//     pub fn new() -> Self {
-//         Self {
-//             pos: Point3::origin(),
-//             target: Point3::origin(),
-//         }
-//     }
-// }
+impl Point for Pointed {}
